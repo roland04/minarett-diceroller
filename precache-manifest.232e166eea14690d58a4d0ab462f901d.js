59bbf1191560b922d64270c55cdf63d44562c0a6
Mikel Martín Corrales <mikel.martin.corrales@gmail.com>
2022-03-31 23:41:12 +0200

    deploy

---
+++ precache-manifest.232e166eea14690d58a4d0ab462f901d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d400d38ca51874d6656",
+    "url": "/minarett-diceroller/css/app.62a0700d.css"
+  },
+  {
+    "revision": "02bba5bc89a7e4a89baa",
+    "url": "/minarett-diceroller/css/chunk-vendors.5d3035bc.css"
+  },
+  {
+    "revision": "fa428d883f25935bf0fa797d270c2948",
+    "url": "/minarett-diceroller/favicon.png"
+  },
+  {
+    "revision": "51aeafa501f2f9bcb03d1926b36d2e86",
+    "url": "/minarett-diceroller/img/face-agi.51aeafa5.png"
+  },
+  {
+    "revision": "e31df449540749190d18e7daced9c5c3",
+    "url": "/minarett-diceroller/img/face-block.e31df449.png"
+  },
+  {
+    "revision": "c65251abde04eca0682a7559a6a8b4b2",
+    "url": "/minarett-diceroller/img/face-hit-agi.c65251ab.png"
+  },
+  {
+    "revision": "fb3ea9763d02308decf3481eb8237452",
+    "url": "/minarett-diceroller/img/face-hit-hit.fb3ea976.png"
+  },
+  {
+    "revision": "d33e4c7cd6996aa925a8e70ef67f34c5",
+    "url": "/minarett-diceroller/img/face-hit-man.d33e4c7c.png"
+  },
+  {
+    "revision": "ff1e3201d52c1b850a945efb4a7ec2f9",
+    "url": "/minarett-diceroller/img/face-hit.ff1e3201.png"
+  },
+  {
+    "revision": "ae0eb84eab83e2661939faab952a1b78",
+    "url": "/minarett-diceroller/img/face-man.ae0eb84e.png"
+  },
+  {
+    "revision": "27dfe145d36c576f8ea1842b5c803d1a",
+    "url": "/minarett-diceroller/img/icon-agi.27dfe145.png"
+  },
+  {
+    "revision": "599ac9467510c4a257b1804e2f0d6e4b",
+    "url": "/minarett-diceroller/img/icon-block.599ac946.png"
+  },
+  {
+    "revision": "d1993faafabc23bb9de0c0ca801f4116",
+    "url": "/minarett-diceroller/img/icon-hit.d1993faa.png"
+  },
+  {
+    "revision": "23cbe6ff7a680838a1350ea2c3af5bbd",
+    "url": "/minarett-diceroller/img/icon-man.23cbe6ff.png"
+  },
+  {
+    "revision": "d1d5493498440f925177954b323815c5",
+    "url": "/minarett-diceroller/img/logo.d1d54934.png"
+  },
+  {
+    "revision": "390bf0c5e280c92997320a962b143bc9",
+    "url": "/minarett-diceroller/index.html"
+  },
+  {
+    "revision": "7d400d38ca51874d6656",
+    "url": "/minarett-diceroller/js/app.3e21b3b1.js"
+  },
+  {
+    "revision": "02bba5bc89a7e4a89baa",
+    "url": "/minarett-diceroller/js/chunk-vendors.80c7c733.js"
+  },
+  {
+    "revision": "e34a53a5f20921720f3d5897e4b7f1ae",
+    "url": "/minarett-diceroller/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/minarett-diceroller/robots.txt"
+  }
+]);
